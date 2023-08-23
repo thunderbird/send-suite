46b1569a5f9465149c1c93c5f0580127eaca82af
Chris Aquino <chris@thunderbird.net>
2023-08-23 07:59:58 -0400

    Re-enable pressing Enter to send message

--- frontend/src/components/MessageSend.vue
+++ frontend/src/components/MessageSend.vue
@@ -91,16 +91,18 @@ async function sendMessage(isText = true) {
   message.value = '';
   fileBlob.value = null;
   msgInput.value.disabled = false;
+  msgInput.value.focus();
 }
 </script>
 
 <template>
-  <input type="file" @change="handleFile" class="hidden" ref="fileInput" />
-  <div v-if="props.conversationId" class="sticky bottom-0">
-    <div class="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0 bg-white">
-      <div class="relative flex">
-        <span class="absolute inset-y-0 flex items-center">
-          <!-- <button
+  <form @submit.prevent>
+    <input type="file" @change="handleFile" class="hidden" ref="fileInput" />
+    <div v-if="props.conversationId" class="sticky bottom-0">
+      <div class="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0 bg-white">
+        <div class="relative flex">
+          <span class="absolute inset-y-0 flex items-center">
+            <!-- <button
             type="button"
             class="inline-flex items-center justify-center rounded-full h-12 w-12 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
           >
@@ -119,36 +121,36 @@ async function sendMessage(isText = true) {
               ></path>
             </svg>
           </button> -->
-        </span>
-        <input
-          type="text"
-          placeholder="Write your message here"
-          class="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-3 bg-gray-200 rounded-md py-3"
-          v-model="message"
-          ref="msgInput"
-        />
-        <div class="absolute right-0 items-center inset-y-0 hidden sm:flex">
-          <button
-            type="button"
-            class="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
-            @click="fileInput.click()"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              class="h-6 w-6 text-gray-600"
+          </span>
+          <input
+            type="text"
+            placeholder="Write your message here"
+            class="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-3 bg-gray-200 rounded-md py-3"
+            v-model="message"
+            ref="msgInput"
+          />
+          <div class="absolute right-0 items-center inset-y-0 hidden sm:flex">
+            <button
+              type="button"
+              class="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
+              @click="fileInput.click()"
             >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
-              ></path>
-            </svg>
-          </button>
-          <!-- <button
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                class="h-6 w-6 text-gray-600"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
+                ></path>
+              </svg>
+            </button>
+            <!-- <button
             type="button"
             class="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
           >
@@ -173,7 +175,7 @@ async function sendMessage(isText = true) {
               ></path>
             </svg>
           </button> -->
-          <!-- <button
+            <!-- <button
             type="button"
             class="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
           >
@@ -192,46 +194,46 @@ async function sendMessage(isText = true) {
               ></path>
             </svg>
           </button> -->
-          <button
-            v-if="fileBlob"
-            type="submit"
-            class="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none"
-            @click="sendMessage(false)"
-          >
-            <span class="font-bold">Upload</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              class="h-6 w-6 ml-2 transform rotate-90"
+            <button
+              v-if="fileBlob"
+              type="submit"
+              class="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none"
+              @click="sendMessage(false)"
             >
-              <path
-                d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"
-              ></path>
-            </svg>
-          </button>
-          <button
-            v-else
-            type="submit"
-            class="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none"
-            @click="sendMessage(true)"
-          >
-            <span class="font-bold">Send</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              class="h-6 w-6 ml-2 transform rotate-90"
+              <span class="font-bold">Upload</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                class="h-6 w-6 ml-2 transform rotate-90"
+              >
+                <path
+                  d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"
+                ></path>
+              </svg>
+            </button>
+            <button
+              v-else
+              type="submit"
+              class="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none"
+              @click="sendMessage(true)"
             >
-              <path
-                d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"
-              ></path>
-            </svg>
-          </button>
+              <span class="font-bold">Send</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                class="h-6 w-6 ml-2 transform rotate-90"
+              >
+                <path
+                  d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"
+                ></path>
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-    <!-- <form @submit.prevent>
+      <!-- <form @submit.prevent>
       <label>
         Message:
         <textarea v-model="message">{{ message }}</textarea>
@@ -244,5 +246,6 @@ async function sendMessage(isText = true) {
         Send Message
       </button>
     </form> -->
-  </div>
+    </div>
+  </form>
 </template>
