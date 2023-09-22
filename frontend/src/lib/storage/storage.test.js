881519a01d80d74cb042b8a094819f4d7b5651d9
Chris Aquino <chris@thunderbird.net>
2023-09-22 09:39:50 -0400

    Confirm that values persist, even with new instances of Storage class

--- frontend/src/lib/storage/storage.test.js
+++ frontend/src/lib/storage/storage.test.js
@@ -2,8 +2,8 @@ import { expect, describe, it } from 'vitest';
 import { Storage } from './index';
 
 describe('User storage', () => {
-  const storage = new Storage();
   it('can store a user without error', async () => {
+    const storage = new Storage();
     const userObj = {
       id: 12345,
       email: 'ned@ryerson.com',
@@ -13,6 +13,7 @@ describe('User storage', () => {
     }).not.toThrowError();
   });
   it('can load the same user', async () => {
+    const storage = new Storage();
     const userObj = {
       id: 12345,
       email: 'ned@ryerson.com',
@@ -23,14 +24,15 @@ describe('User storage', () => {
 });
 
 describe('Key storage', () => {
-  const storage = new Storage();
   it('can store keys', async () => {
+    const storage = new Storage();
     const keys = { 100: 'abc', 102: 'abd', 104: 'abe' };
     expect(async () => {
       await storage.storeKeys(keys);
     }).not.toThrowError();
   });
   it('can retrieve keys', async () => {
+    const storage = new Storage();
     const keys = { 100: 'abc', 102: 'abd', 104: 'abe' };
     await storage.storeKeys(keys);
     const storedKeys = await storage.loadKeys();
