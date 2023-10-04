f75c38cfb66faf22b8a9a5a06c2e5332e502bc5d
Chris Aquino <chris@thunderbird.net>
2023-10-04 17:41:16 -0400

    Can open extension vue page from background.js

--- frontend/public/background.js
+++ frontend/public/background.js
@@ -9,6 +9,13 @@ browser.cloudFile.onFileUpload.addListener(
     console.log(data);
     console.log('------------------------');
 
+    const window = await browser.windows.create({
+      url: browser.runtime.getURL('index.extension.html'),
+      type: 'popup',
+      // Should not be needed: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/windows/create#parameters
+      allowScriptsToClose: true,
+    });
+    console.log(window);
     // for my next trick, I'll open the extension as a popup
     return {
       url: 'https://fakeaddress.comi&thing=' + password.value,
