d5d038bd35e9d2eb2eb99823e496a0e79fd7e854
Chris Aquino <chris@thunderbird.net>
2023-12-21 09:17:54 -0500

    Better handling of prisma migrate for docker

--- backend/scripts/dev-entry.sh
+++ backend/scripts/dev-entry.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 echo 'Running prisma migrations...'
-npm run db:migrate
+npm run db:update
 
-
-echo 'Installing dependencies'
-npm install -g pnpm
-pnpm install
 echo 'Generating prisma client...'
 npm run db:generate
 
