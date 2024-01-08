fd702f48281b1708afd87ca88199949e9569dc79
Chris Aquino <chris@thunderbird.net>
2024-01-08 11:05:44 -0500

    Always show breadcrumbs

--- frontend/src/lockbox/components/Breadcrumbs.vue
+++ frontend/src/lockbox/components/Breadcrumbs.vue
@@ -1,12 +1,12 @@
 <script setup>
-import { ref, onMounted, inject, watchEffect } from 'vue';
+import { ref, inject, watchEffect } from 'vue';
 const { rootFolder, gotoRootFolder } = inject('folderManager');
 
 const path = ref([]);
 
 watchEffect(() => {
   path.value = [rootFolder.value];
-  let parent = rootFolder.value.parent;
+  let parent = rootFolder.value?.parent;
   while (parent) {
     path.value.unshift(parent);
     parent = parent.parent;
@@ -19,7 +19,7 @@ watchEffect(() => {
     <li class="inline-block pl-1">
       <button @click="gotoRootFolder(null)">🏠</button>
     </li>
-    <li v-for="node of path" class="inline-block pl-1">
+    <li v-if="rootFolder" v-for="node of path" class="inline-block pl-1">
       &nbsp;&gt;&nbsp;
       <button @click.prevent="gotoRootFolder(node.id)">
         {{ node.name }}
