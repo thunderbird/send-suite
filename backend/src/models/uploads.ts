22ee8edf7f572c9b43306fa6184f11fe64476259
Chris Aquino <chris@thunderbird.net>
2024-03-18 17:16:56 -0400

    Update uploads.ts to use `fromPrisma` helper

--- backend/src/models/uploads.ts
+++ backend/src/models/uploads.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
+import { fromPrisma } from './prisma-helper';
 
 export async function createUpload(
   id: string,
@@ -7,51 +8,59 @@ export async function createUpload(
   ownerId: number,
   type: string
 ) {
-  try {
-    return await prisma.upload.create({
-      data: {
-        id,
-        size,
-        ownerId,
-        createdAt: new Date(),
-        type,
-      },
-    });
-  } catch (err) {
+  const query = {
+    data: {
+      id,
+      size,
+      ownerId,
+      createdAt: new Date(),
+      type,
+    },
+  };
+  const onError = () => {
     throw new Error(`could not create upload`);
-  }
+  };
+  return await fromPrisma(prisma.upload.create, query, onError);
 }
 
 export async function getUploadSize(id: string) {
-  try {
-    const upload = await prisma.upload.findUniqueOrThrow({
-      where: {
-        id,
-      },
-      select: {
-        size: true,
-      },
-    });
-    return upload.size;
-  } catch (err) {
-    throw new Error(`Could not find upload`);
-  }
+  const query = {
+    where: {
+      id,
+    },
+    select: {
+      size: true,
+    },
+  };
+  const onError = () => {
+    throw new Error(`could not create upload`);
+  };
+  const upload = await fromPrisma(
+    prisma.upload.findUniqueOrThrow,
+    query,
+    onError
+  );
+  return upload.size;
 }
 
 export async function getUploadMetadata(id: string) {
-  try {
-    const upload = await prisma.upload.findUniqueOrThrow({
-      where: {
-        id,
-      },
-      select: {
-        size: true,
-        type: true,
-      },
-    });
-    const { size, type } = upload;
-    return { size, type };
-  } catch (err) {
+  const query = {
+    where: {
+      id,
+    },
+    select: {
+      size: true,
+      type: true,
+    },
+  };
+  const onError = () => {
     throw new Error(`Could not find upload`);
-  }
+  };
+  const upload = await fromPrisma(
+    prisma.upload.findUniqueOrThrow,
+    query,
+    onError
+  );
+  const { size, type } = upload;
+  return { size, type };
 }
