36c3929ad5c87abeac6d5716f4b5de4d86ad0053
Chris Aquino <chris@thunderbird.net>
2024-05-01 16:15:38 -0400

    Use dayjs type for provide/inject

--- frontend/src/apps/lockbox/pages/Web.vue
+++ frontend/src/apps/lockbox/pages/Web.vue
@@ -1,11 +1,12 @@
-<script setup>
+<script setup lang="ts">
 import { provide } from 'vue';
 import Home from '@/apps/lockbox/views/Home.vue';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import { DayJsKey } from '@/types';
 
 dayjs.extend(relativeTime);
-provide('dayjs', dayjs);
+provide(DayJsKey, dayjs);
 </script>
 
 <template>
