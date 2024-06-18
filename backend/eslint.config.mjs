8b917a2c414c8d75ae5f1ef9ed181dccc61997c9
Alejandro Aspinwall <aaspinwall@gmail.com>
2024-06-18 12:12:54 -0400

    chore: Update prettier configuration for consistent code formatting

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,12 +1,11 @@
-import pluginJs from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default [
-    { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    eslintConfigPrettier,
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
 ];
