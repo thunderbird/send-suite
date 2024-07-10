f4e8ae99f9bf6e964dd6a6760f045f0dc99243eb
Chris Aquino <chris@thunderbird.net>
2024-07-10 17:41:13 -0400

    Fix/143 timeout issue with tests (#145)
    * Replace ava with vitest
    
    * Rewrite tests using vitest
    
    * Makes file names unique for backblaze and s3 tests
    
    * Updates the setTimeout code with comment about linux-only and uses Promise to ensure that code runs
    
    * Add code to clean up files created during tests

--- backend/ava.config.mjs
+++
@@ -1,6 +0,0 @@
-// Using the "CommonJS" config from https://typestrong.org/ts-node/docs/recipes/ava/
-export default {
-  files: ['src/test/**/*.test.ts'],
-  extensions: ['ts'],
-  require: ['ts-node/register', 'dotenv/config'],
-};
