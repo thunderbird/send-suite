b66beea8635cff4834ac77812636ed12be90d77f
Alejandro Aspinwall <aaspinwall@gmail.com>
2024-09-05 11:40:34 -0400

    squash refactor dev scripts

---
+++ backend/scripts/entry.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# pnpm install
+
+echo 'Applying prisma migrations...'
+pnpm db:update
+
+echo 'Generating prisma client...'
+pnpm db:generate
+
+
+# Check if DEBUG variable is set in the .env file
+if [ "$DEBUG" = "true" ]; then
+    echo 'Starting dev server with debugger 🚀'
+    echo 'Starting db browser on http://localhost:5555 🔎'
+    pnpm debug 
+else
+    echo 'Starting dev server 🚀'
+    pnpm dev
+fi
