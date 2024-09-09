b42310f4045e9a532ff7fe066ead9e616cabf844
Alejandro Aspinwall <aaspinwall@gmail.com>
2024-09-09 16:51:23 -0400

    add api rewrites

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,52 +1,73 @@
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
-import { defineConfig } from 'vite';
-import { SERVER_BASE_URL } from './config/constants';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    sentryVitePlugin({
-      org: 'thunderbird',
-      project: 'send-suite-frontend',
-      authToken: process.env.VITE_SENTRY_AUTH_TOKEN,
-    }),
-  ],
-  server: {
-    // `https: true` gives `Error code: SSL_ERROR_NO_CYPHER_OVERLAP`
-    // https: true,
-    proxy: {
-      // `secure: false` seems to do nothing
-      // secure: false,
-      '/lockbox/fxa': SERVER_BASE_URL, // Using `backend` per the docker network name
-      '/login-success.html': SERVER_BASE_URL, // Using `backend` per the docker network name
-      '/login-failed.html': SERVER_BASE_URL, // Using `backend` per the docker network name
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const SERVER_BASE_URLS = {
+    /* backend is the docker network name */
+    development: env.VITE_HTTP_SERVER_URL || env.VITE_SEND_SERVER_URL,
+    production: 'https://lockbox.thunderbird.dev',
+  };
+
+  const SERVER_BASE_URL = SERVER_BASE_URLS[mode];
+
+  console.log('env:', env);
+  console.log('Environment:', mode);
+  console.log('Server Base URL:', SERVER_BASE_URL);
+
+  return {
+    plugins: [
+      vue(),
+      sentryVitePlugin({
+        org: 'thunderbird',
+        project: 'send-suite-frontend',
+        authToken: env.VITE_SENTRY_AUTH_TOKEN,
+      }),
+    ],
+    server: {
+      // `https: true` gives `Error code: SSL_ERROR_NO_CYPHER_OVERLAP`
+      // https: true,
+      proxy: {
+        // `secure: false` seems to do nothing
+        // secure: false,
+        '/lockbox/fxa': SERVER_BASE_URL, // Using `backend` per the docker network name
+        '/login-success.html': SERVER_BASE_URL, // Using `backend` per the docker network name
+        '/login-failed.html': SERVER_BASE_URL, // Using `backend` per the docker network name
+        // with options
+        '/api': {
+          target: SERVER_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
     },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      // input: {
-      //   // background: resolve(__dirname, "src/background.js"),
-      //   "extension-ui": resolve(__dirname, "index.extension-ui.html"),
-      //   "extension-settings": resolve(
-      //     __dirname,
-      //     "index.extension-settings.html"
-      //   ),
-      //   // testpage: resolve(__dirname, "index.test.html"),
-      //   // management: resolve(__dirname, "index.management.html"),
-      //   // stats: resolve(__dirname, 'index.stats.html'),
-      //   // options: resolve(__dirname, 'index.options.html'),
-      //   // popup: resolve(__dirname, 'index.popup.html'),
-      // },
+    build: {
+      outDir: 'dist',
+      rollupOptions: {
+        // input: {
+        //   // background: resolve(__dirname, "src/background.js"),
+        //   "extension-ui": resolve(__dirname, "index.extension-ui.html"),
+        //   "extension-settings": resolve(
+        //     __dirname,
+        //     "index.extension-settings.html"
+        //   ),
+        //   // testpage: resolve(__dirname, "index.test.html"),
+        //   // management: resolve(__dirname, "index.management.html"),
+        //   // stats: resolve(__dirname, 'index.stats.html'),
+        //   // options: resolve(__dirname, 'index.options.html'),
+        //   // popup: resolve(__dirname, 'index.popup.html'),
+        // },
+      },
+      sourcemap: true,
     },
-    sourcemap: true,
-  },
+  };
 });
