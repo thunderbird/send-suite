7e3a5b34b95d002136db771eb21efbced6ef526a
Alejandro Aspinwall <aaspinwall@gmail.com>
2024-09-17 13:57:30 -0400

    create frontend scripts for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,53 +1,70 @@
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    sentryVitePlugin({
-      org: 'thunderbird',
-      project: 'send-suite-frontend',
-      authToken: process.env.VITE_SENTRY_AUTH_TOKEN,
-    }),
-  ],
-  server: {
-    // `https: true` gives `Error code: SSL_ERROR_NO_CYPHER_OVERLAP`
-    // https: true,
-    proxy: {
-      // `secure: false` seems to do nothing
-      // secure: false,
-      '/echo': 'http://localhost:8080',
-      '/api': 'http://localhost:8080',
-      '/lockbox/fxa': 'http://backend:8080', // Using `backend` per the docker network name
-      '/login-success.html': 'http://backend:8080', // Using `backend` per the docker network name
-      '/login-failed.html': 'http://backend:8080', // Using `backend` per the docker network name
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const SERVER_BASE_URLS = {
+    /* 
+    backend is the docker network name by default
+    if you want to specify a different server URL, you can do so by setting the VITE_HTTP_SERVER_URL VITE_HTTP_SERVER_URL
+     */
+    development: 'http://backend:8080',
+    production: 'https://lockbox.thunderbird.dev',
+  };
+
+  const SERVER_BASE_URL = SERVER_BASE_URLS[mode];
+
+  console.log('env:', env);
+  console.log('Environment:', mode);
+  console.log('Server Base URL:', SERVER_BASE_URL);
+
+  return {
+    plugins: [
+      vue(),
+      sentryVitePlugin({
+        org: 'thunderbird',
+        project: 'send-suite-frontend',
+        authToken: env.VITE_SENTRY_AUTH_TOKEN,
+      }),
+    ],
+    server: {
+      // `https: true` gives `Error code: SSL_ERROR_NO_CYPHER_OVERLAP`
+      // https: true,
+      proxy: {
+        // `secure: false` seems to do nothing
+        // secure: false,
+        '/lockbox/fxa': SERVER_BASE_URL, // Using `backend` per the docker network name
+        '/login-success.html': SERVER_BASE_URL, // Using `backend` per the docker network name
+        '/login-failed.html': SERVER_BASE_URL, // Using `backend` per the docker network name
+      },
     },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
-  },
-  build: {
-    outDir: 'dist/pages',
-    rollupOptions: {
-      // input: {
-      //   // background: resolve(__dirname, "src/background.js"),
-      //   "extension-ui": resolve(__dirname, "index.extension-ui.html"),
-      //   "extension-settings": resolve(
-      //     __dirname,
-      //     "index.extension-settings.html"
-      //   ),
-      //   // testpage: resolve(__dirname, "index.test.html"),
-      //   // management: resolve(__dirname, "index.management.html"),
-      //   // stats: resolve(__dirname, 'index.stats.html'),
-      //   // options: resolve(__dirname, 'index.options.html'),
-      //   // popup: resolve(__dirname, 'index.popup.html'),
-      // },
+    build: {
+      outDir: 'dist',
+      rollupOptions: {
+        // input: {
+        //   // background: resolve(__dirname, "src/background.js"),
+        //   "extension-ui": resolve(__dirname, "index.extension-ui.html"),
+        //   "extension-settings": resolve(
+        //     __dirname,
+        //     "index.extension-settings.html"
+        //   ),
+        //   // testpage: resolve(__dirname, "index.test.html"),
+        //   // management: resolve(__dirname, "index.management.html"),
+        //   // stats: resolve(__dirname, 'index.stats.html'),
+        //   // options: resolve(__dirname, 'index.options.html'),
+        //   // popup: resolve(__dirname, 'index.popup.html'),
+        // },
+      },
+      sourcemap: true,
     },
-    sourcemap: true,
-  },
+  };
 });
