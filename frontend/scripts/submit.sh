d07a07f7c5e2f9ac69730aa1c79d9c5c75f07f10
Alejandro Aspinwall <aaspinwall@gmail.com>
2024-10-08 13:50:44 -0400

    update publishing scripts and update frontend to v0.1.10 (#249)
    * add submission script and update readme
    
    * update doc to make it more clear
    
    * update submit script

---
+++ frontend/scripts/submit.sh
@@ -0,0 +1,34 @@
+cd ..
+
+mkdir frontend-source
+
+# Copy only necessary files
+cp -r frontend/src frontend-source/src
+cp -r frontend/public frontend-source/public
+cp frontend/package.json frontend-source/package.json
+cp frontend/tsconfig.json frontend-source/tsconfig.json
+cp frontend/.env.production frontend-source/.env.production
+cp frontend/index.extension.html frontend-source/index.extension.html
+cp frontend/index.html frontend-source/index.html
+cp frontend/index.management.html frontend-source/index.management.html
+cp frontend/vite.config.extension.js frontend-source/vite.config.extension.js
+cp frontend/vite.config.js frontend-source/vite.config.js
+cp frontend/vite.config.management.js frontend-source/vite.config.management.js
+cp frontend/tailwind.config.js frontend-source/tailwind.config.js
+cp frontend/postcss.config.js frontend-source/postcss.config.js
+cp frontend/pnpm-lock.yaml frontend-source/pnpm-lock.yaml
+cp frontend/favicon.ico frontend-source/favicon.ico
+cp frontend/README.md frontend-source/README.md
+mkdir frontend-source/scripts
+cp frontend/scripts/build.sh frontend-source/scripts/build.sh
+
+# Create zip for submission
+zip -r frontend-source.zip frontend-source
+# Remove the directory
+rm -rf frontend-source
+
+mv frontend/send-suite-alpha.xpi  send-suite-alpha.xpi
+
+echo "Finished creating frontend-source.zip!"
+
+
