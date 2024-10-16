cef3ffbe4d9a1dd5f51b99b719cd2d6a7a344213
Chris Aquino <chris@thunderbird.net>
2024-10-16 18:21:01 -0400

    Remove unnecessary account storage check, make eslint happy

--- frontend/public/background.js
+++ frontend/public/background.js
@@ -1,9 +1,5 @@
+/// <reference types="thunderbird-webext-browser" />
 console.log('hello from the extension!', new Date().getTime());
-// ==============================================
-// GLOBALS
-// which have been copied over from other files.
-const SERVER = `server`;
-const DEBUG = true;
 
 // ==============================================
 // Account initialization
@@ -11,16 +7,9 @@ const DEBUG = true;
   const allAccounts = await browser.cloudFile.getAllAccounts();
   for (let { id } of allAccounts) {
     console.log(`found an account with id ${id}`);
-    if (await checkAccountStorage(id)) {
-      setAccountConfigured(id);
-    }
+    setAccountConfigured(id);
   }
 })();
-
-async function checkAccountStorage(accountId) {
-  const accountInfo = await browser.storage.local.get(accountId);
-  return accountInfo[accountId] && SERVER in accountInfo[accountId];
-}
 
 function setAccountConfigured(accountId) {
   try {
@@ -29,7 +18,9 @@ function setAccountConfigured(accountId) {
     });
     console.log(`Set ${accountId} as configured:true`);
   } catch (e) {
-    console.log(`setAccountConfigured: You're probably running this outside of Thundebird`);
+    console.log(
+      `setAccountConfigured: You're probably running this outside of Thundebird`
+    );
   }
 }
 
@@ -37,26 +28,28 @@ function setAccountConfigured(accountId) {
 
 // ==============================================
 // Handle cloudFile attachments
-browser.cloudFile.onFileUpload.addListener(async (account, { id, name, data }) => {
-  console.log(`🐈 here are account, id, name, and data`);
-  console.log(account);
-  console.log(id);
-  console.log(name);
-  console.log(data);
-  console.log('------------------------');
+browser.cloudFile.onFileUpload.addListener(
+  async (account, { id, name, data }) => {
+    console.log(`🐈 here are account, id, name, and data`);
+    console.log(account);
+    console.log(id);
+    console.log(name);
+    console.log(data);
+    console.log('------------------------');
 
-  await browser.windows.create({
-    url: browser.runtime.getURL('index.extension.html'),
-    type: 'popup',
-    allowScriptsToClose: true,
-  });
+    await browser.windows.create({
+      url: browser.runtime.getURL('index.extension.html'),
+      type: 'popup',
+      allowScriptsToClose: true,
+    });
 
-  return createMessageHandler(account, id, name, data);
-});
+    return createMessageHandler(account, id, name, data);
+  }
+);
 
 function createMessageHandler(account, id, name, data) {
   return new Promise((resolve, reject) => {
-    async function handleMessage(message, sender) {
+    async function handleMessage(message) {
       const { type, url, aborted } = message;
 
       switch (type) {
@@ -94,88 +87,3 @@ function createMessageHandler(account, id, name, data) {
     browser.runtime.onMessage.addListener(handleMessage);
   });
 }
-
-// ==============================================
-// Handle composeAction
-// (flow for browsing Lockbox and inserting link into email)
-async function insertLink(tab, href = 'https://some.share.link/hash-goes-here', text = 'file-name-goes-here.md') {
-  // Get the existing message.
-  let details = await browser.compose.getComposeDetails(tab.id);
-  console.log(details);
-
-  if (details.isPlainText) {
-    // The message is being composed in plain text mode.
-    let body = details.plainTextBody;
-    console.log(body);
-
-    // Make direct modifications to the message text, and send it back to the editor.
-    // body += "\n\nSent from my Thunderbird";
-    body += '\n\n' + href;
-    console.log(body);
-    browser.compose.setComposeDetails(tab.id, { plainTextBody: body });
-  } else {
-    // The message is being composed in HTML mode. Parse the message into an HTML document.
-    let document = new DOMParser().parseFromString(details.body, 'text/html');
-    console.log(document);
-
-    // Use normal DOM manipulation to modify the message.
-    let link = document.createElement('a');
-    link.setAttribute('href', href);
-    link.textContent = text;
-    document.body.appendChild(link);
-    document.body.appendChild(document.createElement('br'));
-
-    // Serialize the document back to HTML, and send it back to the editor.
-    let html = new XMLSerializer().serializeToString(document);
-    console.log(html);
-    browser.compose.setComposeDetails(tab.id, { body: html });
-  }
-}
-// async function handleComposeActionMessage(message, sender) {
-//   const { type, fileId } = message;
-//   switch (type) {
-//     case 'FILE_SELECTED':
-//       console.log(`🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀`);
-//       console.log(type);
-//       console.log(fileId);
-//       break;
-//     default:
-//       break;
-//   }
-// }
-
-// browser.composeAction.onClicked.addListener(async (tab) => {
-//   // onFileUpload needs a promise.
-//   // would I benefit from one here?
-//   browser.runtime.onMessage.addListener(async (message, sender) => {
-//     const { type, url } = message;
-//     switch (type) {
-//       case 'SELECTION_COMPLETE':
-//         console.log(type);
-//         console.log(url);
-
-//         // might need to do all my encryption and whatnot from...
-//         // another component.
-//         // Can Share do the thing?
-//         // Right now, it can share a single item
-//         // theoretically, it can share multiple
-//         // but also, it's made for rendering
-
-//         insertLink(tab, url, 'download from lockbox');
-//         break;
-//       default:
-//         break;
-//     }
-//   });
-//   console.log(`Added the composeAction listener`);
-
-//   console.log(`Opening the Lockbox UI in a popup`);
-//   // insertLink(tab);
-//   const popup = await browser.windows.create({
-//     url: browser.runtime.getURL('index.extension.html'),
-//     type: 'popup',
-//     allowScriptsToClose: true,
-//   });
-// });
-
-// ==============================================
