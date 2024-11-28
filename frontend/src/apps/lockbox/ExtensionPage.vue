6524a91a65b248625a2a301aba82296606db86bd
Alejandro Aspinwall <aaspinwall@gmail.com>
2024-11-28 13:07:25 -0500

    make popup nice too

--- frontend/src/apps/lockbox/ExtensionPage.vue
+++ frontend/src/apps/lockbox/ExtensionPage.vue
@@ -9,6 +9,9 @@ import { onMounted } from 'vue';
 import { useMetricsUpdate } from '@/apps/common/mixins/metrics';
 import useApiStore from '@/stores/api-store';
 import useMetricsStore from '@/stores/metrics';
+import SecureSendIcon from '../common/SecureSendIcon.vue';
+import StatusBar from '../common/StatusBar.vue';
+import TBBanner from '../common/TBBanner.vue';
 import PopupView from './views/PopupView.vue';
 
 const userStore = useUserStore();
@@ -29,6 +32,29 @@ useMetricsUpdate();
 </script>
 
 <template>
-  <PopupView />
-  <FeedbackBox />
+  <div class="container">
+    <TBBanner />
+    <PopupView />
+    <FeedbackBox />
+    <SecureSendIcon />
+    <StatusBar />
+  </div>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem 0;
+  margin-top: 2rem;
+}
+p {
+  color: #000;
+  font-size: 13px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+</style>
