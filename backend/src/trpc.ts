283855bd507ca72811acfe80fcac732a1164a830
Alejandro Aspinwall <aaspinwall@gmail.com>
2024-12-09 17:50:50 -0500

    clean comments

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -9,19 +9,3 @@ const t = initTRPC.context<Context>().create();
 export const router = t.router;
 export const publicProcedure = t.procedure;
 export const mergeRouters = t.mergeRouters;
-
-// const contextTest = async ({ ctx, next }) => {
-//   console.log('ctx', ctx);
-//   return next({
-//     ctx: { user: 'yix' },
-//   });
-// };
-
-// publicProcedure.use(contextTest);
-
-/**
- * Export reusable router and procedure helpers
- * that can be used throughout the router
- */
-
-// export type AppRouter =
