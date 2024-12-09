05e4dbb73fbb13a3412eb923ba68a9fd1dada490
Alejandro Aspinwall <aaspinwall@gmail.com>
2024-12-09 10:42:21 -0500

    only install deps on prod

--- backend/scripts/entry.sh
+++ backend/scripts/entry.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 
-# Create zip for submission
-if [ "$IS_CI_AUTOMATION" != "yes" ]; then
-    echo 'Skipping lockfile install on CI'    
+
+# Check if environment NODE_ENV has been set to production
+if [ "$NODE_ENV" = "production" ]; then
+    echo 'Starting with NODE_ENV on production 🐧'
 else
     # *IS* CI automation
     echo 'installing backend deps 🤖'
     pnpm install --frozen-lockfile
 fi
 
-# Check if environment NODE_ENV has been set to production
-if [ "$NODE_ENV" = "production" ]; then
-    echo 'Starting with NODE_ENV on production 🐧'
-fi
 
 echo 'Applying prisma migrations...'
 pnpm db:update
