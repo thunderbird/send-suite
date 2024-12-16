6cbe8e4e0e471626f16db7e6d2dd6e269e1e0104
Alejandro Aspinwall <aaspinwall@gmail.com>
2024-12-16 12:15:10 -0600

    track downloads from frontend (#436)

--- frontend/src/apps/lockbox/stores/folder-store.ts
+++ frontend/src/apps/lockbox/stores/folder-store.ts
@@ -18,6 +18,8 @@ import { NamedBlob } from '@/lib/filesync';
 import { backupKeys } from '@/lib/keychain';
 import { CLIENT_MESSAGES } from '@/lib/messages';
 import { checkBlobSize, formatBlob } from '@/lib/utils';
+
+import useMetricsStore from '@/stores/metrics';
 import { useStatusStore } from './status-store';
 
 export interface FolderStore {
@@ -58,6 +60,7 @@ const useFolderStore: () => FolderStore = defineStore('folderManager', () => {
   const { api } = useApiStore();
   const { user } = useUserStore();
   const { setUploadSize, setProgress, progress } = useStatusStore();
+  const { metrics } = useMetricsStore();
   const { keychain } = useKeychainStore();
 
   const uploader = new Uploader(user, keychain, api);
@@ -276,7 +279,8 @@ const useFolderStore: () => FolderStore = defineStore('folderManager', () => {
       uploadId,
       containerId,
       wrappedKeyStr,
-      name
+      name,
+      metrics
     );
   }
 
