a6fc0a84c1775d4234790b8d139bc1eaf3698ebb
Alejandro Aspinwall <aaspinwall@gmail.com>
2025-01-14 09:08:36 -0500

    add copy tooltip to access links (#441)

--- frontend/src/apps/lockbox/extension.js
+++ frontend/src/apps/lockbox/extension.js
@@ -2,6 +2,8 @@ import '@/lib/logger';
 import { initSentry } from '@/lib/sentry';
 import posthogPlugin from '@/plugins/posthog';
 import { VueQueryPlugin } from '@tanstack/vue-query';
+import FloatingVue from 'floating-vue';
+import 'floating-vue/dist/style.css';
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 
@@ -16,4 +18,5 @@ initSentry(app);
 app.use(VueQueryPlugin);
 app.use(pinia);
 app.use(posthogPlugin);
+app.use(FloatingVue);
 app.mount('#extension-page');
--- frontend/src/apps/lockbox/lockbox.js
+++ frontend/src/apps/lockbox/lockbox.js
@@ -3,11 +3,14 @@ import { initSentry } from '@/lib/sentry';
 import posthogPlugin from '@/plugins/posthog';
 import { VueQueryPlugin } from '@tanstack/vue-query';
 import '@thunderbirdops/services-ui/style.css';
+import FloatingVue from 'floating-vue';
+import 'floating-vue/dist/style.css';
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import Lockbox from './LockboxPage.vue';
 import router from './router';
 import './style.css';
+
 const pinia = createPinia();
 const app = createApp(Lockbox);
 
@@ -17,4 +20,5 @@ app.use(VueQueryPlugin);
 app.use(pinia);
 app.use(router);
 app.use(posthogPlugin);
+app.use(FloatingVue);
 app.mount('#app');
