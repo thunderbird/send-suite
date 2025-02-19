ff0ed78bfbed121cc2c118ed060e65acf6beb23b
Alejandro Aspinwall <aaspinwall@gmail.com>
2025-02-19 15:20:55 -0500

    add staging tag to xpi

--- frontend/scripts/config.ts
+++ frontend/scripts/config.ts
@@ -4,6 +4,9 @@ dotenv.config();
 export const ID_FOR_PROD = `"id": "tb-send@thunderbird.net"`;
 export const ID_FOR_STAGING = ` "id": "send@thunderbird.net"`;
 
+export const NAME_FOR_PROD = `"name": "Thunderbird Send"`;
+export const NAME_FOR_STAGING = `"name": "Thunderbird Send [STAGING]"`;
+
 export const PACKAGE_NAME = {
   production: 'tb-send',
   staging: 'send',
