6fffc7b86516f8d3c1f17c602385800e72374733
Alejandro Aspinwall <aaspinwall@gmail.com>
2025-03-04 13:57:00 -0500

    warn when posthog keys are not set correctly

--- backend/src/metrics/index.ts
+++ backend/src/metrics/index.ts
@@ -1,3 +1,4 @@
+import { getEnvironmentName } from '@/config';
 import { PostHog } from 'posthog-node';
 
 const client = new PostHog(process.env.POSTHOG_API_KEY || 'test', {
@@ -5,7 +6,19 @@ const client = new PostHog(process.env.POSTHOG_API_KEY || 'test', {
 });
 
 export const useMetrics = () => {
-  if (!process.env.POSTHOG_API_KEY || !process.env.POSTHOG_HOST) {
+  const isProd = getEnvironmentName() === 'prod';
+
+  if (
+    !process.env.POSTHOG_API_KEY ||
+    !process.env.POSTHOG_HOST ||
+    process.env.POSTHOG_API_KEY === 'test' ||
+    process.env.POSTHOG_HOST === 'test'
+  ) {
+    if (isProd) {
+      console.error(
+        `POSTHOG keys not correctly set, we got POSTHOG_API_KEY: ${process.env.POSTHOG_API_KEY} and POSTHOG_HOST: ${process.env.POSTHOG_HOST}`
+      );
+    }
     console.warn('POSTHOG keys not set');
   }
   return client;
