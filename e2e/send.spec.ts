613de3b388a6aacf5a30368838cce68a5b5c27f4
Alejandro Aspinwall <aaspinwall@gmail.com>
2025-03-14 14:55:39 -0400

    cleanup tests

--- e2e/send.spec.ts
+++ e2e/send.spec.ts
@@ -1,98 +1,70 @@
 import { BrowserContext, expect, firefox, Page, test } from "@playwright/test";
-import { readFileSync } from "fs";
+import fs, { readFileSync } from "fs";
+import path from "path";
 
-// const storageStatePath = path.resolve(__dirname, "../data/lockboxstate.json");
+const storageStatePath = path.resolve(__dirname, "../data/state.json");
 const TIMEOUT = 3_000;
-
-// console.log(storageStatePath);
 
 const password = `qghp392784rq3rgqp329r@$`;
 const email = `myemail${Date.now()}@tb.pro`;
 
-async function setup() {
-  const browser = await firefox.launch();
-
-  const context = await browser.newContext({
-    ignoreHTTPSErrors: true,
-    // storageState: storageStatePath,
-  });
-
-  const page = await context.newPage();
-
-  // Check that local cert is trusted
-  await page.goto("https://localhost:8088");
-  expect(await page.content()).toContain("echo");
-
-  await page.goto("http://localhost:5173/send");
-
-  const registerButton = await page.getByTestId("register-button");
-
-  await registerButton.click();
-
-  const emailField = await page.getByTestId("email");
-  const passwordField = await page.getByTestId("password");
-  const confirmPasswordField = await page.getByTestId("confirm-password");
-
-  await emailField.fill(email);
-  await passwordField.fill(password);
-  await confirmPasswordField.fill(password);
-
-  const submitButton = await page.getByTestId("submit-button");
-  await submitButton.click();
-
-  const logOutButton = await page.getByTestId("log-out-button");
-  await logOutButton.click();
-
-  page.on("dialog", (dialog) => dialog.accept());
-
-  // log back in
-  await emailField.fill(email);
-  await passwordField.fill(password);
-  const submitLogin = await page.getByTestId("login-submit-button");
-  await submitLogin.click();
-
-  const backupKeysButton = await page.getByTestId("encrypt-keys-button");
-  await backupKeysButton.click();
-
-  return { context, page };
-}
-
-const dragAndDropFile = async (
-  page: Page,
-  selector: string,
-  filePath: string,
-  fileName: string,
-  fileType = ""
-) => {
-  // print current path
-  console.log("current path: ", __dirname);
-  const buffer = readFileSync(__dirname + filePath).toString("base64");
-
-  const dataTransfer = await page.evaluateHandle(
-    async ({ bufferData, localFileName, localFileType }) => {
-      const dt = new DataTransfer();
-
-      const blobData = await fetch(bufferData).then((res) => res.blob());
-
-      const file = new File([blobData], localFileName, { type: localFileType });
-      dt.items.add(file);
-      return dt;
-    },
-    {
-      bufferData: `data:application/octet-stream;base64,${buffer}`,
-      localFileName: fileName,
-      localFileType: fileType,
-    }
-  );
-
-  await page.dispatchEvent(selector, "drop", { dataTransfer });
+type Props = {
+  context: BrowserContext;
+  page: Page;
 };
 
-test("Register and log in", async () => {
-  const { context, page } = await setup();
+(async () => {
+  test.describe.configure({ mode: "serial", retries: 3 });
+  test.beforeAll(async () => {
+    const { context, page } = await setup_browser();
 
-  await expect(page).toHaveTitle(/Thunderbird Send/);
+    // Go to main page
+    await page.goto("http://localhost:5173/send");
 
+    await register_and_login({ context, page });
+  });
+
+  test.afterAll(async () => {
+    fs.writeFileSync(
+      storageStatePath,
+      JSON.stringify({
+        cookies: [],
+        origins: [],
+      })
+    );
+  });
+
+  test.beforeEach(async () => {
+    await setup_browser();
+  });
+
+  test("Renders files page", async () => {
+    const { page } = await setup_browser();
+
+    await page.goto("https://localhost:8088");
+    expect(await page.content()).toContain("echo");
+
+    // Go to main page
+    await page.goto("http://localhost:5173/send");
+
+    expect(
+      await page.getByRole("heading", { name: "Your Files" }).textContent()
+    ).toBe("Your Files");
+  });
+
+  test("Upload workflow", async () => {
+    const { page, context } = await setup_browser();
+
+    // Go to main page
+    await page.goto("http://localhost:5173/send");
+
+    await expect(page).toHaveTitle(/Thunderbird Send/);
+
+    await upload_workflow({ page, context });
+  });
+})();
+
+async function upload_workflow({ page }: Props) {
   const profileButton = page.getByRole("link", { name: "My Files" });
 
   await new Promise((resolve) => setTimeout(resolve, TIMEOUT));
@@ -156,15 +128,87 @@ test("Register and log in", async () => {
   await page.waitForURL("**/send/profile");
 
   expect(await page.getByText("Tier: FREE").textContent()).toBe("Tier: FREE");
+}
+
+async function register_and_login({ page, context }: Props) {
+  const registerButton = await page.getByTestId("register-button");
+
+  await registerButton.click();
+
+  const emailField = await page.getByTestId("email");
+  const passwordField = await page.getByTestId("password");
+  const confirmPasswordField = await page.getByTestId("confirm-password");
+
+  await emailField.fill(email);
+  await passwordField.fill(password);
+  await confirmPasswordField.fill(password);
+
+  const submitButton = await page.getByTestId("submit-button");
+  await submitButton.click();
+
+  const logOutButton = await page.getByTestId("log-out-button");
+  await logOutButton.click();
+
+  page.on("dialog", (dialog) => dialog.accept());
+
+  // log back in
+  await emailField.fill(email);
+  await passwordField.fill(password);
+  const submitLogin = await page.getByTestId("login-submit-button");
+  await submitLogin.click();
+
+  const backupKeysButton = await page.getByTestId("encrypt-keys-button");
+  await backupKeysButton.click();
 
   await saveStorage(context);
-});
+}
+
+async function setup_browser() {
+  const browser = await firefox.launch();
+
+  const context = await browser.newContext({
+    ignoreHTTPSErrors: true,
+    storageState: storageStatePath,
+  });
+
+  const page = await context.newPage();
+
+  return { context, page };
+}
+
+const dragAndDropFile = async (
+  page: Page,
+  selector: string,
+  filePath: string,
+  fileName: string,
+  fileType = ""
+) => {
+  // print current path
+  console.log("current path: ", __dirname);
+  const buffer = readFileSync(__dirname + filePath).toString("base64");
+
+  const dataTransfer = await page.evaluateHandle(
+    async ({ bufferData, localFileName, localFileType }) => {
+      const dt = new DataTransfer();
+
+      const blobData = await fetch(bufferData).then((res) => res.blob());
+
+      const file = new File([blobData], localFileName, { type: localFileType });
+      dt.items.add(file);
+      return dt;
+    },
+    {
+      bufferData: `data:application/octet-stream;base64,${buffer}`,
+      localFileName: fileName,
+      localFileType: fileType,
+    }
+  );
+
+  await page.dispatchEvent(selector, "drop", { dataTransfer });
+};
 
 async function saveStorage(context: BrowserContext) {
-  return;
-  // temporary disabled
-  // console.log("saving context");
-  // await context.storageState({
-  //   path: `./data/lockbox${new Date().toISOString().toString()}.json`,
-  // });
+  await context.storageState({
+    path: `./data/state.json`,
+  });
 }
