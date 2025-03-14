f8e3207bf2b6c3209823ca68864826796730b9e5
Alejandro Aspinwall <aaspinwall@gmail.com>
2025-03-14 15:01:28 -0400

    cleanup tests

--- e2e/send.spec.ts
+++ e2e/send.spec.ts
@@ -1,14 +1,19 @@
-import { BrowserContext, expect, firefox, Page, test } from "@playwright/test";
-import fs, { readFileSync } from "fs";
+import { BrowserContext, expect, Page, test } from "@playwright/test";
+import fs from "fs";
 import path from "path";
+import {
+  register_and_login,
+  setup_browser,
+  upload_workflow,
+} from "./testUtils";
 
-const storageStatePath = path.resolve(__dirname, "../data/state.json");
-const TIMEOUT = 3_000;
+export const storageStatePath = path.resolve(__dirname, "../data/state.json");
+const emptyState = {
+  cookies: [],
+  origins: [],
+};
 
-const password = `qghp392784rq3rgqp329r@$`;
-const email = `myemail${Date.now()}@tb.pro`;
-
-type Props = {
+export type PlaywrightProps = {
   context: BrowserContext;
   page: Page;
 };
@@ -17,21 +22,13 @@ type Props = {
   test.describe.configure({ mode: "serial", retries: 3 });
   test.beforeAll(async () => {
     const { context, page } = await setup_browser();
-
     // Go to main page
     await page.goto("http://localhost:5173/send");
-
     await register_and_login({ context, page });
   });
 
   test.afterAll(async () => {
-    fs.writeFileSync(
-      storageStatePath,
-      JSON.stringify({
-        cookies: [],
-        origins: [],
-      })
-    );
+    fs.writeFileSync(storageStatePath, JSON.stringify(emptyState));
   });
 
   test.beforeEach(async () => {
@@ -63,152 +60,3 @@ type Props = {
     await upload_workflow({ page, context });
   });
 })();
-
-async function upload_workflow({ page }: Props) {
-  const profileButton = page.getByRole("link", { name: "My Files" });
-
-  await new Promise((resolve) => setTimeout(resolve, TIMEOUT));
-
-  await profileButton.click();
-
-  // Select folder
-  let folder = page.getByTestId("folder-row");
-  await folder.click();
-
-  const sharelinkButton = page.getByTestId("create-share-link");
-  sharelinkButton.click();
-
-  const createdShareLink = page.getByTestId("link-0");
-  expect(await createdShareLink.inputValue()).toContain("/share/");
-
-  // Open folder
-  folder = page.getByTestId("folder-row");
-
-  await folder.click();
-
-  // Find upload box
-  const dropzoneBox = page.getByTestId("drop-zone");
-
-  expect(await dropzoneBox.textContent({ timeout: TIMEOUT })).toContain(
-    "files here to upload"
-  );
-
-  await dragAndDropFile(page, "#drop-zone", "/test.txt", "test.txt");
-
-  await page.getByTestId("upload-button").click();
-
-  await page.waitForSelector(`[data-testid="folder-table-row-cell"]`);
-
-  expect(await page.getByTestId("file-count").textContent()).toBe("1");
-
-  const responsePromise = page.waitForResponse(
-    (response) => response.request().method() === "DELETE"
-  );
-
-  // Delete file
-  await page.getByTestId("delete-file").click({ force: true });
-
-  // Wait for DELETE request to complete
-  await responsePromise;
-
-  expect((await responsePromise).status()).toBe(200);
-
-  expect(await page.getByTestId("file-count").isVisible()).toBeFalsy();
-
-  // Go to the root
-  await page.getByTestId("home-button").click();
-
-  await folder.click();
-  await folder.click();
-
-  expect(await page.getByTestId("file-count").isVisible()).toBeFalsy();
-
-  page.getByRole("link", { name: "Profile" }).click();
-
-  await page.waitForURL("**/send/profile");
-
-  expect(await page.getByText("Tier: FREE").textContent()).toBe("Tier: FREE");
-}
-
-async function register_and_login({ page, context }: Props) {
-  const registerButton = await page.getByTestId("register-button");
-
-  await registerButton.click();
-
-  const emailField = await page.getByTestId("email");
-  const passwordField = await page.getByTestId("password");
-  const confirmPasswordField = await page.getByTestId("confirm-password");
-
-  await emailField.fill(email);
-  await passwordField.fill(password);
-  await confirmPasswordField.fill(password);
-
-  const submitButton = await page.getByTestId("submit-button");
-  await submitButton.click();
-
-  const logOutButton = await page.getByTestId("log-out-button");
-  await logOutButton.click();
-
-  page.on("dialog", (dialog) => dialog.accept());
-
-  // log back in
-  await emailField.fill(email);
-  await passwordField.fill(password);
-  const submitLogin = await page.getByTestId("login-submit-button");
-  await submitLogin.click();
-
-  const backupKeysButton = await page.getByTestId("encrypt-keys-button");
-  await backupKeysButton.click();
-
-  await saveStorage(context);
-}
-
-async function setup_browser() {
-  const browser = await firefox.launch();
-
-  const context = await browser.newContext({
-    ignoreHTTPSErrors: true,
-    storageState: storageStatePath,
-  });
-
-  const page = await context.newPage();
-
-  return { context, page };
-}
-
-const dragAndDropFile = async (
-  page: Page,
-  selector: string,
-  filePath: string,
-  fileName: string,
-  fileType = ""
-) => {
-  // print current path
-  console.log("current path: ", __dirname);
-  const buffer = readFileSync(__dirname + filePath).toString("base64");
-
-  const dataTransfer = await page.evaluateHandle(
-    async ({ bufferData, localFileName, localFileType }) => {
-      const dt = new DataTransfer();
-
-      const blobData = await fetch(bufferData).then((res) => res.blob());
-
-      const file = new File([blobData], localFileName, { type: localFileType });
-      dt.items.add(file);
-      return dt;
-    },
-    {
-      bufferData: `data:application/octet-stream;base64,${buffer}`,
-      localFileName: fileName,
-      localFileType: fileType,
-    }
-  );
-
-  await page.dispatchEvent(selector, "drop", { dataTransfer });
-};
-
-async function saveStorage(context: BrowserContext) {
-  await context.storageState({
-    path: `./data/state.json`,
-  });
-}
---
+++ e2e/testUtils.ts
@@ -0,0 +1,156 @@
+import { BrowserContext, expect, firefox, Page } from "@playwright/test";
+import { readFileSync } from "fs";
+import { PlaywrightProps, storageStatePath } from "./send.spec";
+
+const TIMEOUT = 3_000;
+const password = `qghp392784rq3rgqp329r@$`;
+const email = `myemail${Date.now()}@tb.pro`;
+
+export async function upload_workflow({ page }: PlaywrightProps) {
+  const profileButton = page.getByRole("link", { name: "My Files" });
+
+  await new Promise((resolve) => setTimeout(resolve, TIMEOUT));
+
+  await profileButton.click();
+
+  // Select folder
+  let folder = page.getByTestId("folder-row");
+  await folder.click();
+
+  const sharelinkButton = page.getByTestId("create-share-link");
+  sharelinkButton.click();
+
+  const createdShareLink = page.getByTestId("link-0");
+  expect(await createdShareLink.inputValue()).toContain("/share/");
+
+  // Open folder
+  folder = page.getByTestId("folder-row");
+
+  await folder.click();
+
+  // Find upload box
+  const dropzoneBox = page.getByTestId("drop-zone");
+
+  expect(await dropzoneBox.textContent({ timeout: TIMEOUT })).toContain(
+    "files here to upload"
+  );
+
+  await dragAndDropFile(page, "#drop-zone", "/test.txt", "test.txt");
+
+  await page.getByTestId("upload-button").click();
+
+  await page.waitForSelector(`[data-testid="folder-table-row-cell"]`);
+
+  expect(await page.getByTestId("file-count").textContent()).toBe("1");
+
+  const responsePromise = page.waitForResponse(
+    (response) => response.request().method() === "DELETE"
+  );
+
+  // Delete file
+  await page.getByTestId("delete-file").click({ force: true });
+
+  // Wait for DELETE request to complete
+  await responsePromise;
+
+  expect((await responsePromise).status()).toBe(200);
+
+  expect(await page.getByTestId("file-count").isVisible()).toBeFalsy();
+
+  // Go to the root
+  await page.getByTestId("home-button").click();
+
+  await folder.click();
+  await folder.click();
+
+  expect(await page.getByTestId("file-count").isVisible()).toBeFalsy();
+
+  page.getByRole("link", { name: "Profile" }).click();
+
+  await page.waitForURL("**/send/profile");
+
+  expect(await page.getByText("Tier: FREE").textContent()).toBe("Tier: FREE");
+}
+
+export async function register_and_login({ page, context }: PlaywrightProps) {
+  const registerButton = await page.getByTestId("register-button");
+
+  await registerButton.click();
+
+  const emailField = await page.getByTestId("email");
+  const passwordField = await page.getByTestId("password");
+  const confirmPasswordField = await page.getByTestId("confirm-password");
+
+  await emailField.fill(email);
+  await passwordField.fill(password);
+  await confirmPasswordField.fill(password);
+
+  const submitButton = await page.getByTestId("submit-button");
+  await submitButton.click();
+
+  const logOutButton = await page.getByTestId("log-out-button");
+  await logOutButton.click();
+
+  page.on("dialog", (dialog) => dialog.accept());
+
+  // log back in
+  await emailField.fill(email);
+  await passwordField.fill(password);
+  const submitLogin = await page.getByTestId("login-submit-button");
+  await submitLogin.click();
+
+  const backupKeysButton = await page.getByTestId("encrypt-keys-button");
+  await backupKeysButton.click();
+
+  await saveStorage(context);
+}
+
+export async function setup_browser() {
+  const browser = await firefox.launch();
+
+  const context = await browser.newContext({
+    ignoreHTTPSErrors: true,
+    storageState: storageStatePath,
+  });
+
+  const page = await context.newPage();
+
+  return { context, page };
+}
+
+const dragAndDropFile = async (
+  page: Page,
+  selector: string,
+  filePath: string,
+  fileName: string,
+  fileType = ""
+) => {
+  // print current path
+  console.log("current path: ", __dirname);
+  const buffer = readFileSync(__dirname + filePath).toString("base64");
+
+  const dataTransfer = await page.evaluateHandle(
+    async ({ bufferData, localFileName, localFileType }) => {
+      const dt = new DataTransfer();
+
+      const blobData = await fetch(bufferData).then((res) => res.blob());
+
+      const file = new File([blobData], localFileName, { type: localFileType });
+      dt.items.add(file);
+      return dt;
+    },
+    {
+      bufferData: `data:application/octet-stream;base64,${buffer}`,
+      localFileName: fileName,
+      localFileType: fileType,
+    }
+  );
+
+  await page.dispatchEvent(selector, "drop", { dataTransfer });
+};
+
+export async function saveStorage(context: BrowserContext) {
+  await context.storageState({
+    path: `./data/state.json`,
+  });
+}
