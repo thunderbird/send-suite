165bf396821b92e953cc376dd64e1c0afb8f44a5
Alejandro Aspinwall <aaspinwall@gmail.com>
2025-03-31 16:27:54 -0400

    use logger on event emitters

--- packages/send/backend/src/ws/login.ts
+++ packages/send/backend/src/ws/login.ts
@@ -1,13 +1,12 @@
+import { logger } from '@/utils/logger';
 import { EventEmitter } from 'ws';
 
 export const loginEmitter = new EventEmitter();
 
 loginEmitter.on('login_complete', () =>
-  console.log('Login complete event received')
+  logger.log('Login complete event received')
 );
 loginEmitter.on('login_attempt', () =>
-  console.log('Login attempt event received')
+  logger.log('Login attempt event received')
 );
-loginEmitter.on('login_url_requested', () =>
-  console.log('Login url requested')
-);
+loginEmitter.on('login_url_requested', () => logger.log('Login url requested'));
